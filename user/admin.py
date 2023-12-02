f59836c24ff764a9011e0c67238795fcc6115626
SNG66 <johnygutierrez1995@gmail.com>
2023-12-02 16:13:47 -0400

    update 4:13 PM 12/2/2023

--- user/admin.py
+++ user/admin.py
@@ -8,10 +8,16 @@ class CustomUserAdmin(UserAdmin):
     form = CustomUserForm
 
     add_fieldsets = (
-        (None, {
-            'fields': ('username', 'first_name', 'last_name', 'email', 'date_of_birth', 'password1', 'password2'),
-        }),
-    )
+    ("Username", {
+        'fields': ('username',),
+    }),
+    ("Personal information", {
+        'fields': ('first_name', 'last_name', 'email', 'date_of_birth'),
+    }),
+    ("Password", {
+        'fields': ('password1', 'password2')
+    }),
+)
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
@@ -26,3 +32,28 @@ admin.site.register(CustomUser, CustomUserAdmin)
 
 # Then I register the CustomUser model with the CustomUserAdmin configuration enabling the admin panel to manage CustomUser instances using the customized admin interface.
 
+
+
+
+
+# Structure of the "add_fieldsets":
+
+
+# Outer Tuple: ( ... )
+
+# This contains a single element, which is an single inner tuple or multiple tuples.
+
+
+
+# Inner Tuple/s: (None/STR, { ... })
+
+# The inner tuple contains two elements:
+# The first element is used for specifying a legend or grouping (can be a string to label this section in the admin panel or set to None).
+# The second element is a dictionary { ... }, defining the configuration for this fieldset.
+
+
+
+# Dictionary:
+
+# This specifies the fields to display in the admin panel when adding a new user. 
+# Each field within the tuple will be displayed in the order provided.
